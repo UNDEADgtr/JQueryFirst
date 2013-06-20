39926dbb762b42c6d88c0732eb768540fbdffa24
UNDEADgtr <undead_gtr@mail.ru>
2013-06-20 17:31:50 +0300

    Added json model

--- src/main/java/by/undead/jquery/Main.java
+++ src/main/java/by/undead/jquery/Main.java
@@ -1,6 +1,11 @@
 package by.undead.jquery;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 import javax.faces.bean.ManagedBean;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  * User: Khralovich Dzmitry
@@ -10,13 +15,31 @@ import javax.faces.bean.ManagedBean;
 @ManagedBean(name = "main")
 public class Main {
 
-    private String value = "This editor is provided by PrimeFaces";
 
-    public String getValue() {
-        return value;
+    public void writeJson() {
+        JSONObject obj = new JSONObject();
+        obj.put("name", "mkyong.com");
+        obj.put("age", new Integer(100));
+
+        JSONArray list = new JSONArray();
+        list.add("msg 1");
+        list.add("msg 2");
+        list.add("msg 3");
+
+        obj.put("messages", list);
+
+        try {
+
+            FileWriter file = new FileWriter("/home/dzmity/IdeaProjects/JQueryFirst/src/main/webapp/resources/json/test.json");
+            file.write(obj.toJSONString());
+            file.flush();
+            file.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.print(obj);
     }
 
-    public void setValue(String value) {
-        this.value = value;
-    }
 }
